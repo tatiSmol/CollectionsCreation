2085cb8e7f4d3d9741528f0fb752e820f1a37c4c
tatiSmol <judeclapton@icloud.com>
2023-12-12 00:34:39 +0300

    get() & remove() methods are written

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -45,21 +45,38 @@ public class MyArrayList<E> {
         size++;
     }
 
-    public boolean add(E element) {
+    public void add(E element) {
         add(size, element);
-        return true;
     }
 
+    @SuppressWarnings("unchecked")
     public E get(int index) {
-        return null;
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index " + index + " out of bounds for length " + size);
+        }
+
+        return (E) elements[index];
     }
 
-    public E remove(int index) {
-        return null;
+    public void remove(int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index " + index + " out of bounds for length " + size);
+        }
+
+        System.arraycopy(elements, index + 1, elements, index, size - index - 1);
+        elements[--size] = null;
     }
 
-    public boolean remove(E element) {
-        return true;
+    public void remove(E element) {
+        int index = Integer.MIN_VALUE;
+        for (int i = 0; i < elements.length; i++) {
+            if (elements[i] == element) {
+                index = i;
+            }
+        }
+        if (index >= 0) {
+            remove(index);
+        }
     }
 
     public boolean addAll(Collection<? extends E> collection) {
